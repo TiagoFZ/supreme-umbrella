bb46d8bf3196a09c458c4c957183c50488b46f6e
Tiago Fiori <tiagozf2@gmail.com>
2022-04-12 15:36:10 -0300

    Go merge

--- hello.php
+++ hello.php
@@ -1,5 +1,5 @@
 <?php
 echo "This is an echo test";
-echo "Adding another line after going to sleep at 5 am";
+echo "Adding another line after asdokasdko going to sleep at 5 am";
 echo "This is a new line of echo test";
 ?>
